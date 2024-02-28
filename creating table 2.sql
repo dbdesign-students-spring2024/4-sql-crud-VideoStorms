49f315eff6d60c019bd5ad72a9103748c7d7bc51
“Rares <raresh.grecu@gmail.com>
2024-02-28 14:53:55 -0500

    Finishing up drafting

--- creating table 2.sql
+++ creating table 2.sql
@@ -13,12 +13,12 @@ CREATE TABLE users (
 
 CREATE TABLE posts (
     post_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id INTEGER,
+    user_id INTEGER NOT NULL,
     post_type TEXT CHECK(post_type IN ('Message', 'Story')) NOT NULL,
     content TEXT NOT NULL,
     recipient_id INTEGER,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    is_visible BOOLEAN NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    is_visible BOOLEAN DEFAULT 1 NOT NULL,
     FOREIGN KEY(user_id) REFERENCES users(user_id),
     FOREIGN KEY(recipient_id) REFERENCES users(user_id)
 );
